5686ff2e6b75cbd3f3607eb6c938f9ee52d18a71
Anita <campbellanitak@gmail.com>
2016-04-29 12:50:58 -0400

    adding box element

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,27 +1,42 @@
 (function() {
   // the minimum version of jQuery we want
-  var v = "1.3.2";
-  // check prior inclusion and version
+  var v = '1.3.2';
   if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
     var done = false;
-    var script = document.createElement("script");
-    script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v +
-      "/jquery.min.js";
+    var script = document.createElement('script');
+    script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/' + v +
+      '/jquery.min.js';
     script.onload = script.onreadystatechange = function() {
-      if (!done && (!this.readyState || this.readyState == "loaded" || this
-        .readyState == "complete")) {
+      if (!done && (!this.readyState || this.readyState == 'loaded' || this
+        .readyState == 'complete')) {
         done = true;
         releaseTheKraken();
       }
     };
-    document.getElementsByTagName("head")[0].appendChild(script);
+    document.getElementsByTagName('head')[0].appendChild(script);
   } else {
     releaseTheKraken();
   }
 
   function releaseTheKraken() {
     window.theKraken = function() {
-      // var outterDiv = document.createElement('div');
+      window.onbeforeunload = function() {
+          return "Are you sure you want to leave? Think of the kittens!";
+        }
+        // create the element:
+      var element = $('<div id="yb_box"></div>');
+      // append it to the body:
+      $('body').append(element);
+      // style it:
+      element.css({
+        position: 'absolute',
+        top: '10px',
+        right: '10px',
+        width: '200px',
+        height: '90px',
+        backgroundColor: 'black'
+      });
+      var outterDiv = document.createElement('div');
       console.log('it works!');
     }();
   }
