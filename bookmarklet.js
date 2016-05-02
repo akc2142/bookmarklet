21cb53bcd61c9b6cb599b2bd296462a12ed23d48
Anita <campbellanitak@gmail.com>
2016-05-02 16:34:51 -0400

    styling

--- bookmarklet.js
+++ bookmarklet.js
@@ -63,7 +63,7 @@
       }
       var slotCriteria = yieldbot.getSlotCriteria();
       var pageCriteria = yieldbot.getPageCriteria();
-      var element = $('<div id="yb_box"> <span style="font-size: 20px; color: rgb(0,153,0);"> theKRAKEN <img src="https://raw.githubusercontent.com/akc2142/bookmarklet/master/yb.png"> </span> <div class="yb_div"> Pub ID is:   <span style="color:rgb(153,255,153); font-weight: normal;">' + pub + '</span></div> <div class="yb_div"> Async is enabled:   ' + asyncEnabled + '</div> <div class="yb_div"> Intent tag is loaded:   ' + ybGo + '</div> <div class="yb_div"><a href="https://ui.yieldbot.com/ui/meow/publisher/'+pvi+'">PVI:   ' + pvi + '</a></div><div class="yb_div">' + '</div>');
+      var element = $('<div id="yb_box"> <span style="font-size: 20px; color: rgb(0,153,0);"> theKRAKEN <img src="https://raw.githubusercontent.com/akc2142/bookmarklet/master/yb.png"> </span> <div class="yb_div"> PVI is: <span style="color:rgb(153,255,153); font-weight: normal;">' + pvi + '</span></div> <div class="yb_div"> Async is enabled: ' + asyncEnabled + '</div> <div class="yb_div"> Intent tag is loaded: ' + ybGo + '</div> <div class="yb_div"><a href="https://ui.yieldbot.com/ui/meow/publisher/'+pub+'"> Pub ID: ' + pub + '</a></div><div class="yb_div">' + '</div>');
       // append it to the body:
       $('body').append(element);
       // style it:
